3d4c8305ac75aac5c8902f76981c78078da51793
El_H <139109199+ElaineH310@users.noreply.github.com>
2025-10-27 14:07:35 +0800

    Add disinfectant calculation logic in 消毒殺菌.cpp

---
+++ 消毒殺菌.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>  // 萬用標頭檔
+using namespace std;         // 命名空間
+void best(){
+  ios::sync_with_stdio(0);  
+  cin.tie(0);  
+  cout.tie(0);  // 輸出入優化
+  ios::sync_with_stdio(false); 
+  cin.tie(NULL);
+}
+int main(){
+  best();
+  int nEntry, yTime, Ttime, Xunit, total=0;
+  cin >> nEntry >> yTime;
+  for(int i=0; i<nEntry; i++){
+    cin >> Ttime >> Xunit;
+    if (i==0){
+      yTime+=Ttime;
+    }
+    int decrease = yTime - Ttime;
+    if (decrease >= 0){
+      Xunit -= decrease;
+      if (Xunit > 0){
+        total += Xunit;
+      }
+    }
+  }
+  cout << total;
+}

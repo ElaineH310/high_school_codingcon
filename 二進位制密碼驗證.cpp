f89f72192534b380e848092e043aff323d40f095
El_H <139109199+ElaineH310@users.noreply.github.com>
2025-10-27 09:48:08 +0800

    Implement binary password validation logic

---
+++ 二進位制密碼驗證.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int zeros=0, ones=0;
+bool checkfive(string pwd) {
+    for (int i = 0; i <= (int) pwd.length() - 3; i++) {
+        if (pwd[i] == '1' && pwd[i+1] == '0' && pwd[i+2] == '1') {
+            return true;
+        }
+    }
+    return false;
+}
+void checkall(string pwd){
+  if (pwd.length()<8||pwd.length()>12) cout<<2;
+  if (zeros<2) cout<<3;
+  if (ones<3) cout<<4;
+  if ((checkfive(pwd))==false) cout<<5;
+  else cout<<0;
+}
+  
+int main(){
+;
+  string pwd;
+  cin>>pwd;
+  for (int i=0;i<pwd.length();i++){
+    if (pwd[i]=='0'){
+      zeros++;
+    }else if (pwd[i]=='1'){
+      ones++;
+    }else{
+      cout<<1;
+      break;
+    }
+  }
+  checkall(pwd);
+  }

a762bd2e2a4f9e4b5d2091d130d1c27e0e9d909e
El_H <139109199+ElaineH310@users.noreply.github.com>
2025-10-27 11:11:50 +0800

    Add income analysis program for data input and sum

---
+++ #include <bits/收入分析.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X, Y;
+    cin >> X >> Y;
+
+    vector<vector<int>> income(Y, vector<int>(X));
+    for (int y = 0; y < Y; y++) {
+        for (int x = 0; x < X; x++) {
+            cin >> income[y][x];
+        }
+    }
+
+    int N;
+    cin >> N;
+
+    for (int q = 0; q < N; q++) {
+        int x1, x2, y1, y2;
+        cin >> x1 >> x2 >> y1 >> y2;
+
+        int sum = 0;
+        for (int y = y1; y <= y2; y++) {
+            for (int x = x1; x <= x2; x++) {
+                sum += income[y][x];
+            }
+        }
+        cout << sum << " ";
+    }
+}

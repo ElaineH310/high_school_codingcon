5e5a1af4a6a489ca016a057c9f2f9777e0e49b30
El_H <139109199+ElaineH310@users.noreply.github.com>
2025-10-31 11:38:21 +0800

    Implement weight calculation program in C++

---
+++ 量體重.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+void best(){
+  ios::sync_with_stdio(0);  
+  cin.tie(0);  
+  cout.tie(0); 
+  cin.tie(NULL);
+}
+int main(){
+  best();
+  vector <int> weightdbl;
+  vector <int> weights(5);
+  int sum=0;
+  int weight;
+  for (int i=0;i<10;i++){
+    cin>>weight;
+    weightdbl.push_back(weight);
+  }
+  sort (weightdbl.begin(), weightdbl.end());
+  for (int j=0;j<10;j++){
+    sum+=weightdbl[j];
+  }
+  weights[2]=sum/4-(weightdbl.front()+weightdbl.back());
+  weights[0]=weightdbl[1]-weights[2];
+  weights[1]=weightdbl[0]-weights[0];
+  weights[4]=weightdbl[8]-weights[2];
+  weights[3]=weightdbl[9]-weights[4];
+  cout<<weights[0]<<" "<<weights[1]<<" "<<weights[2]<<" "<<weights[3]<<" "<<weights[4]<<endl;
+}
